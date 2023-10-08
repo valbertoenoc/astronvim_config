c05ad1f444d1743e2ae60b01de79db998b680359
Valberto Enoc <valbertoenoc@gmail.com>
2023-10-07 22:56:15 -0300

    temporary removal of typescript config; changed default theme to kanagawa

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "kanagawa",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
